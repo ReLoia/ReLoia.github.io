7dd4cb92ea8cfb7129493f2e8912fa932825b892
ReLoia <zrennyz@gmail.com>
2023-06-05 00:24:17 +0200

    Fixes and Added stuff Added transition between parts of the page Added content to index.html Added not playing to the music player Moved showcase \- Added information (age, location, etc.) \- Added projects \- Started favourite artists

--- base.js
+++ base.js
@@ -1,11 +1,15 @@
 
 // Elements
 const timeCont = document.querySelector('[head] > div[info] t');
-// const title = document.querySelector('curtain [title]');
+const etaEl = document.querySelector('age');
 
-// title.onclick = () => {
-// 	title.parentElement.style.transform = 'translateY(-100vh)'
-// }
+// Setup stuff
+etaEl.innerText = Math.floor((new Date() - new Date('2006-02-13')) / 31104000000);
+
+// Main 1s async interval
+setInterval(async () => {
+	timeCont.innerText = `${new Date().toLocaleString("en-US", { timeZone: "Europe/Rome", hour: "numeric", minute: "numeric" })}`
+}, 1000)
 
 const queries = location.search.slice(1).split('&').map(el => /(.+)=(.+)/g.exec(el));
 ((url, info) => {
@@ -15,28 +19,40 @@ const queries = location.search.slice(1).split('&').map(el => /(.+)=(.+)/g.exec(
 	document.querySelector('showcase p t').innerText = (info ?? '').replace(/%20/g, ' ');
 })((queries.length ? queries.find(el => el?.[1] == 'image') : null)?.[2], (queries.length ? queries.find(el => el?.[1] == 'info') : null)?.[2])
 
-// Main 1s async interval
-setInterval(async ()=> {
-	timeCont.innerText = `${new Date().toLocaleString("en-US", {timeZone: "Europe/Rome", hour: "numeric", minute: "numeric"})}`
-}, 1000)
-
 // Premere qualasiasi tasto per continuare
 // Fare che non si può scrollare il body
-
-let pageActivated = false;
-document.onwheel = (e) => {
-	// console
-	if (!pageActivated) return e.preventDefault();
-	if (e.deltaY < 0 && window.scrollY == 0) {
+let pageActivated = false
+let lastClientY = 0;
+let moving = false;
+const movingHandler = (e) => {
+	const delta = 'deltaY' in e ? e.deltaY : lastClientY - e.touches[0].clientY;
+	lastClientY = 'deltaY' in e ? 0 : e.touches[0].clientY;
+	moving = true;
+	setTimeout(() => moving = false, 1000);
+	if (delta < 0 && window.scrollY == 0) {
 		pageActivated = false;
+		document.body.style.position = 'fixed';
 		document.querySelector('body').style.transform = 'translateY(0)'
+	} else if (delta > 0 && window.scrollY == 0) activatePage(e);
+}
+document.onwheel = document.ontouchmove = movingHandler;
+document.body.style.position = 'fixed';
+const activatePage = (e) => {
+	document.onkeydown = null;
+	document.onmousedown = null;
+	if (moving) return moving = false;
+
+	if (e) {
+		if ('key' in e) {
+			if (e.key == 'Tab' || e.key == 'Alt') return;
+		} else {
+			if (['A', 'SVG'].includes(e.target.nodeName)) return;
+		}
 	}
-}
-document.onkeydown = () => {
 	pageActivated = true;
+	document.body.style.position = 'unset';
 	document.querySelector('body').style.transform = 'translateY(-100vh)'
-}
-document.onmousedown = () => {
-	pageActivated = true;
-	document.querySelector('body').style.transform = 'translateY(-100vh)'
-}
+};
+document.onkeydown = activatePage;
+document.onmousedown = activatePage;
+document.ontouchend = activatePage;
